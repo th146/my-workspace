f72b91be22c7df24ca22beb3e3a19269cd976a4b
th146 <149154222+th146@users.noreply.github.com>
2024-11-08 19:23:09 +0100

    update
    update

---
+++ apps/workshop-ui/src/app/appointments.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AppointmentListComponent } from './appointments.component';
+
+describe('AppointmentListComponent', () => {
+  let component: AppointmentListComponent;
+  let fixture: ComponentFixture<AppointmentListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AppointmentListComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppointmentListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ apps/workshop-ui/src/app/appointments.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-appointment-list',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<p>appointment-list works!</p>`,
+  styles: ``,
+})
+export class AppointmentListComponent {}
